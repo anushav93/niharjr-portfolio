3d60cf5f64d2b6b9a49f152e05d6863a174f2d83
Uday Vatti <uvatti@labelbox.com>
2024-08-21 12:36:48 -0500

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import Image from "next/image";
 import Layout from "@/components/Layout";
+
 
 
 
 export default function Home() {
   return (
   <Layout>
-    <h1>Nihar reddy j </h1>
+    <div className="h-screen flex flex-col justify-center bg-white">
+      <div className="grid grid-cols-12">
+        <div className="col-span-12 md:col-span-3">
+          <div className="">
+            
+          </div>
+        </div>
+        <div className="col-span-12 md:col-span-6">
+          <div className="flex flex-col justify-center items-center h-full">
+            <h1 className="text-4xl font-bold text-gray-800">Welcome to Next.js</h1>
+            <p className="text-gray-600">A React framework for building production grade applications.</p>
+          </div>
+      </div>
+      </div>
+    </div>
   </Layout>
   );
 }
