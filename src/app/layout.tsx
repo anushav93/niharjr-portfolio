a908ab99ea4430a4fc8d3c12b84a814655e45b07
anusha v <anushaventrapragada93@gmail.com>
2025-08-26 20:21:45 -0500

    added clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "t157v20ode");
+            `,
+          }}
+        />
+      </head>
       {/* Ensure Tailwind uses the sans stack */}
       <body className={inter.className}>
         <Providers>
